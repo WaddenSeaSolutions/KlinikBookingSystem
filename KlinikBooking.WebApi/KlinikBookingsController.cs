9b0a1efea301731beb4bfa63b8496315aab6e4c3
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2025-12-16 09:49:30 +0100

    save

---
+++ KlinikBooking.WebApi/KlinikBookingsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KlinikBooking.Core;
+using KlinikBooking.Core.Interfaces;
+
+namespace KlinikBooking.WebApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KlinikBookingsController
+    {
+
+        private IRepository<Booking> bookingRepository;
+
+
+
+
+
+    }
+}
