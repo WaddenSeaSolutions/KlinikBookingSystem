cfd920f5d9c34ea527978043d3da2555354ac7ed
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2025-12-17 09:47:19 +0100

    Adds booking repository and configures API
    Implements a booking repository for data access.
    Configures the API with necessary services and Swagger.
    It also initializes an in-memory database with sample data for development.

---
+++ KlinikBooking.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,52 @@
+﻿using KlinikBooking.Core.Entitites;
+using KlinikBooking.Core.Interfaces;
+using KlinikBooking.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HotelBooking.Infrastructure.Repositories
+{
+    public class BookingRepository : IRepository<Booking>
+    {
+        private readonly KlinikBookingContext db;
+
+        public BookingRepository(KlinikBookingContext context)
+        {
+            db = context;
+        }
+
+        public async Task AddAsync(Booking entity)
+        {
+            db.Booking.Add(entity);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task EditAsync(Booking entity)
+        {
+            db.Entry(entity).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<Booking> GetAsync(int id)
+        {
+            return await db.Booking.Include(b => b.Patient).
+                Include(b => b.TreatmentRoom).
+                FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<IEnumerable<Booking>> GetAllAsync()
+        {
+            return await db.Booking.Include(b => b.Patient).
+                Include(b => b.TreatmentRoom).
+                ToListAsync();
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var booking = await db.Booking.FirstOrDefaultAsync(b => b.Id == id);
+            db.Booking.Remove(booking);
+            await db.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ KlinikBooking.WebApi/program.cs
@@ -0,0 +1,52 @@
+﻿
+using Castle.Core.Resource;
+using KlinikBooking.Core;
+using KlinikBooking.Core.Entitites;
+using KlinikBooking.Core.Interfaces;
+using KlinikBooking.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<k>(opt => 
+    opt.UseInMemoryDatabase("HotelBookingDb"));
+
+builder.Services.AddScoped<IRepository<TreatmentRoom>, TreatmentRoomRepository>();
+builder.Services.AddScoped<IRepository<Patient>, PatientRepository>();
+builder.Services.AddScoped<IRepository<Booking>, BookingRepository>();
+builder.Services.AddScoped<IBookingManager, BookingManager>();
+builder.Services.AddTransient<IDbinitializer, Dbinitializer>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // Initialize the database.
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetService<HotelBookingContext>();
+        var dbInitializer = services.GetService<IDbinitializer>();
+        dbInitializer.Initialize(dbContext);
+    }
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
