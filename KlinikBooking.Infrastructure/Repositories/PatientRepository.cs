971fbdff53780e304a5d446ecd22c31215d17ffd
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2025-12-17 09:58:44 +0100

    Adds repositories for Patient and Room
    Adds PatientRepository and TreatmentRoomRepository
    which provide data access methods using Entity Framework Core.
    The PatientRepository implements GetAllAsync and GetAsync methods.
    The TreatmentRoomRepository implements AddAsync, GetAllAsync,
    GetAsync, and RemoveAsync methods.

---
+++ KlinikBooking.Infrastructure/Repositories/PatientRepository.cs
@@ -0,0 +1,41 @@
+﻿using KlinikBooking.Core.Entitites;
+using KlinikBooking.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace KlinikBooking.Infrastructure.Repositories
+{
+    public class PatientRepository : IRepository<Patient>
+    {
+        private readonly KlinikBookingContext db;
+
+        public PatientRepository(KlinikBookingContext context)
+        {
+            db = context;
+        }
+        public Task AddAsync(Patient entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(Patient entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Patient>> GetAllAsync()
+        {
+            return await db.Patient.ToListAsync();
+
+        }
+
+        public async Task<Patient> GetAsync(int id)
+        {
+            return await db.Patient.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public Task RemoveAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
